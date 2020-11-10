6f2e818a3c544a1124ca41086147910202d96d2a
Blake A. Long <blake.a.long@gmail.com>
2020-11-10 11:26:55 -0700

    Spring Boot starter dependencies

---
+++ src/test/java/com/blakelong/employeemanagementsystem/EmployeemanagementsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blakelong.employeemanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeemanagementsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
